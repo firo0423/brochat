356fcf3f83677e388f486a7590e25f944f9e6419
firo <cfiro@qq.com>
2022-01-07 15:40:47 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VueSocketIO from "vue-socket.io";
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+// 配置socket.io
+Vue.use(
+  new VueSocketIO({
+    debug: true,
+    connection: "http://localhost:4000",
+    options:{autoConnect:false}
+  })
+);
+
 
 new Vue({
   router,
